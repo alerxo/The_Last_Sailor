84320bdf0f643ecbddcd70f7b55a86aba2f938d1
alerxo <alexoorobin@gmail.com>
2024-10-28 19:31:50 +0100

    Buoyancy Rework and Engine Script

--- Assets/Boat/Buoyancy.cs
+++ Assets/Boat/Buoyancy.cs
@@ -3,18 +3,17 @@ using UnityEngine.Rendering.HighDefinition;
 
 public class Bouyancy : MonoBehaviour
 {
-    [SerializeField] private float Strength = 2f;
-    [SerializeField] private float Depth = 1f;
-    [SerializeField] private float AerialDrag = 0.01f;
-    [SerializeField] private float SubmergedDrag = 1f;
+    [SerializeField] private float bouyancy = 1f;
+    [SerializeField] private float hullDepth = 1f;
 
     private WaterSurface waterSurface;
     private WaterSearchParameters waterSearchParameters = new();
     private WaterSearchResult waterSearchResult = new();
 
     [SerializeField] private Transform[] points;
-
     private Rigidbody target;
+
+    [SerializeField] private float averageSubmergedDepth, averageSubmergedPercentage, totalDisplacement;
 
     private void Awake()
     {
@@ -24,33 +23,33 @@ public class Bouyancy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float submerged = 0f;
+        averageSubmergedDepth = 0f;
+        averageSubmergedPercentage = 0f;
+        totalDisplacement = 0f;
 
         foreach (Transform point in points)
         {
-            Debug.DrawLine(point.position, point.position + (Vector3.up * 5), Color.red, Time.fixedDeltaTime);
-
             waterSearchParameters.startPositionWS = waterSearchResult.candidateLocationWS;
             waterSearchParameters.targetPositionWS = point.position;
             waterSearchParameters.error = 0.01f;
             waterSearchParameters.maxIterations = 8;
             waterSurface.ProjectPointOnWaterSurface(waterSearchParameters, out waterSearchResult);
 
-            target.AddForceAtPosition(Physics.gravity / points.Length, point.position, ForceMode.Acceleration);
+            float submergedDepth = point.position.y - waterSearchResult.projectedPositionWS.y;
+            float submergedPercentage = -(Mathf.Clamp(submergedDepth, -hullDepth, 0) / hullDepth);
 
-            float depth = point.position.y - waterSearchResult.projectedPositionWS.y;
-
-            if (depth < 0)
+            if (submergedPercentage > 0)
             {
-                float displacement = Mathf.Clamp01((waterSearchResult.projectedPositionWS.y - point.position.y) / Depth) * Strength;
-                target.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacement, 0f), point.position, ForceMode.Acceleration);
-                target.AddForce(displacement * Time.fixedDeltaTime * -target.linearVelocity, ForceMode.VelocityChange);
-                target.AddTorque(displacement * Time.fixedDeltaTime * -target.angularVelocity, ForceMode.VelocityChange);
+                float displacement = submergedPercentage * target.mass * bouyancy / points.Length;
+                target.AddForceAtPosition(new Vector3(0f, displacement, 0f), point.position, ForceMode.Force);
 
-                submerged += 1f / points.Length;
+                totalDisplacement += displacement;
             }
-        }
 
-        target.linearDamping = Mathf.Lerp(AerialDrag, SubmergedDrag, submerged);
+            averageSubmergedPercentage += submergedPercentage / points.Length;
+            averageSubmergedDepth += submergedDepth / points.Length;
+
+            Debug.DrawLine(point.position, point.position + (Vector3.up * hullDepth), Color.red, Time.fixedDeltaTime);
+        }
     }
 }
