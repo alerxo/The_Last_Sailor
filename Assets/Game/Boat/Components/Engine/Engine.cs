d94db2919e3859ffe82b9d72ff84ad1e9572922d
alerxo <110595790+alerxo@users.noreply.github.com>
2024-11-11 19:28:41 +0100

    New Model and animations

--- Assets/Game/Boat/Components/Engine/Engine.cs
+++ Assets/Game/Boat/Components/Engine/Engine.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Engine : MonoBehaviour
@@ -8,6 +9,8 @@ public class Engine : MonoBehaviour
     [SerializeField] private float acceleration;
     [SerializeField] private float turnRadius;
     [SerializeField] private float turnSpeed;
+
+    [SerializeField] private Transform steeringWheel, leftWheel, rightWheel;
 
     private Rigidbody target;
 
@@ -30,6 +33,7 @@ public class Engine : MonoBehaviour
         if (throttle > 0)
         {
             target.AddForceAtPosition(throttle * transform.forward, transform.position, ForceMode.Force);
+            RotateWheel(leftWheel, throttle, 0.002f);
         }
 
 #if UNITY_EDITOR
@@ -40,9 +44,22 @@ public class Engine : MonoBehaviour
 #endif
     }
 
+    private void RotateWheel(Transform wheel, float throttle, float degree)
+    {
+        wheel.transform.Rotate(new Vector3(throttle * degree * Time.deltaTime, 0, 0));
+    }
+
     public void ChangeMovement(Vector2 _movement)
     {
-        if (_movement.x != 0) movement.x = Mathf.Lerp(movement.x, -_movement.x, turnSpeed * Time.deltaTime);
-        if (_movement.y != 0) movement.y = Mathf.Lerp(movement.y, _movement.y, acceleration * Time.deltaTime);
+        if (_movement.x != 0)
+        {
+            movement.x = Mathf.Lerp(movement.x, -_movement.x, turnSpeed * Time.deltaTime);
+            steeringWheel.transform.Rotate(new Vector3(movement.x * 100 * Time.deltaTime, 0, 0));
+        }
+
+        if (_movement.y != 0)
+        {
+            movement.y = Mathf.Lerp(movement.y, _movement.y, acceleration * Time.deltaTime);
+        }
     }
 }
