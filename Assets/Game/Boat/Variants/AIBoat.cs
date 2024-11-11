27188bbd94967df039e1217af259e05e068a3711
alerxo <110595790+alerxo@users.noreply.github.com>
2024-11-11 18:36:33 +0100

    AI Pathfinding

--- Assets/Game/Boat/Variants/AIBoat.cs
+++ Assets/Game/Boat/Variants/AIBoat.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class AIBoat : Boat
 {
     [SerializeField] private Cannon[] leftCannons, rightCannons;
+    private Vector3? destination;
+    private const float APROACH_DISTANCE = 20f;
+
+    private float distance;
+    private Vector3 cross;
 
     public override void Destroyed()
     {
@@ -11,10 +16,33 @@ public class AIBoat : Boat
 
     private void Update()
     {
-        ChangeMovement(new Vector2(1, 1));
-        ChangeCannonAngle(new Vector2(0, 1));
+        SetDestination(FindFirstObjectByType<PlayerBoat>().transform.position);
+        ChangeMovement(GetMovementDirection());
         FireLeft();
         FireRight();
+    }
+
+    private Vector2 GetMovementDirection()
+    {
+        Vector2 movement = Vector2.zero;
+
+        if (destination == null)
+        {
+            return movement;
+        }
+
+        distance = Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(destination.Value.x, destination.Value.z));
+        movement.y = distance < APROACH_DISTANCE ? distance / APROACH_DISTANCE : 1;
+
+        cross = Vector3.Cross((transform.position - destination.Value).normalized, transform.forward);
+        movement.x = cross.y;
+
+        return movement;
+    }
+
+    public void SetDestination(Vector3 _destination)
+    {
+        destination = _destination;
     }
 
     protected void ChangeCannonAngle(Vector2 _rotation)
