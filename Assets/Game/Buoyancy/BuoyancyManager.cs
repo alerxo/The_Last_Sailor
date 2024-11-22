9a118bd47edeb573c7889bb72577b884a849e0cd
alerxo <alexoorobin@gmail.com>
2024-11-22 18:27:45 +0100

    Buoyancy Manager Optimized

--- Assets/Game/Buoyancy/BuoyancyManager.cs
+++ Assets/Game/Buoyancy/BuoyancyManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using Unity.Jobs;
@@ -10,9 +11,26 @@ public class BuoyancyManager : MonoBehaviour
 {
     public static BuoyancyManager Instance { get; private set; }
 
+    private const int MAX_UPDATE_COUNT = 25;
+    private const int MEDIUM_QUALITY_UPDATE_COUNT = 5;
+    private const int LOW_QUALITY_UPDATE_COUNT = 1;
+
     [SerializeField] private BuoyancyPoints buoyancyPoints;
+
     private readonly List<Buoyancy> targets = new();
-    private int currentTarget;
+
+    private readonly List<Buoyancy> highQuality = new();
+    private readonly List<Buoyancy> mediumQuality = new();
+    private readonly List<Buoyancy> lowQuality = new();
+
+    private int highQualityIndex = 0;
+    private int mediumQualityIndex = 0;
+    private int lowQualityIndex = 0;
+
+    private const int HIGH_QUALITY_RANGE = 500;
+    private const int MEDIUM_QUALITY_RANGE = 1000;
+
+    private bool isQualityCheckRunning = false;
 
     private WaterSurface waterSurface;
 
@@ -24,6 +42,8 @@ public class BuoyancyManager : MonoBehaviour
     private NativeArray<float3> normalBuffer;
     private NativeArray<float3> directionBuffer;
 
+    private Transform player;
+
     private void Awake()
     {
         Assert.IsNull(Instance);
@@ -31,33 +51,129 @@ public class BuoyancyManager : MonoBehaviour
 
         waterSurface = FindFirstObjectByType<WaterSurface>();
 
-        targetPositionBuffer = new(buoyancyPoints.Values.Length, Allocator.Persistent);
-        projectedPositionBuffer = new(buoyancyPoints.Values.Length, Allocator.Persistent);
-        errorBuffer = new(buoyancyPoints.Values.Length, Allocator.Persistent);
-        candidatePositionBuffer = new(buoyancyPoints.Values.Length, Allocator.Persistent);
-        stepCountBuffer = new(buoyancyPoints.Values.Length, Allocator.Persistent);
-        normalBuffer = new(buoyancyPoints.Values.Length, Allocator.Persistent);
-        directionBuffer = new(buoyancyPoints.Values.Length, Allocator.Persistent);
+        targetPositionBuffer = new(buoyancyPoints.Values.Length * MAX_UPDATE_COUNT, Allocator.Persistent);
+        projectedPositionBuffer = new(buoyancyPoints.Values.Length * MAX_UPDATE_COUNT, Allocator.Persistent);
+        errorBuffer = new(buoyancyPoints.Values.Length * MAX_UPDATE_COUNT, Allocator.Persistent);
+        candidatePositionBuffer = new(buoyancyPoints.Values.Length * MAX_UPDATE_COUNT, Allocator.Persistent);
+        stepCountBuffer = new(buoyancyPoints.Values.Length * MAX_UPDATE_COUNT, Allocator.Persistent);
+        normalBuffer = new(buoyancyPoints.Values.Length * MAX_UPDATE_COUNT, Allocator.Persistent);
+        directionBuffer = new(buoyancyPoints.Values.Length * MAX_UPDATE_COUNT, Allocator.Persistent);
 
         AddTarget(FindFirstObjectByType<Buoyancy>());
     }
 
+    private void Start()
+    {
+        player = PlayerBoatController.Instance.transform;
+    }
+
     private void Update()
     {
-        if (targets.Count > 0)
+        if (!isQualityCheckRunning)
         {
-            if (currentTarget >= targets.Count)
+            isQualityCheckRunning = true;
+            StartCoroutine(CheckQuality());
+        }
+
+        GetTargets();
+        UpdateTargetsDepthValues();
+        targets.Clear();
+    }
+
+    private IEnumerator CheckQuality()
+    {
+        for (int i = 0; i < lowQuality.Count; i++)
+        {
+            float distance = Vector3.Distance(player.position, lowQuality[i].transform.position);
+
+            if (distance <= MEDIUM_QUALITY_RANGE)
             {
-                currentTarget = 0;
+                mediumQuality.Add(lowQuality[i]);
+                lowQuality.RemoveAt(i);
+                i--;
             }
 
-            UpdateTargetDepthValues();
+            yield return null;
+        }
 
-            currentTarget++;
+        for (int i = 0; i < mediumQuality.Count; i++)
+        {
+            float distance = Vector3.Distance(player.position, mediumQuality[i].transform.position);
+
+            if (distance > MEDIUM_QUALITY_RANGE)
+            {
+                lowQuality.Add(mediumQuality[i]);
+                mediumQuality.RemoveAt(i);
+                i--;
+            }
+
+            else if (distance <= HIGH_QUALITY_RANGE)
+            {
+                highQuality.Add(mediumQuality[i]);
+                mediumQuality.RemoveAt(i);
+                i--;
+            }
+
+            yield return null;
+        }
+
+        for (int i = 0; i < highQuality.Count; i++)
+        {
+            float distance = Vector3.Distance(player.position, highQuality[i].transform.position);
+
+            if (distance > HIGH_QUALITY_RANGE)
+            {
+                mediumQuality.Add(highQuality[i]);
+                highQuality.RemoveAt(i);
+                i--;
+            }
+
+            yield return null;
+        }
+
+        isQualityCheckRunning = false;
+    }
+
+    private void GetTargets()
+    {
+        for (int i = targets.Count; i < LOW_QUALITY_UPDATE_COUNT; i++)
+        {
+            if (lowQualityIndex >= lowQuality.Count)
+            {
+                lowQualityIndex = 0;
+                break;
+            }
+
+            targets.Add(lowQuality[lowQualityIndex]);
+            lowQualityIndex++;
+        }
+
+        for (int i = targets.Count; i < MEDIUM_QUALITY_UPDATE_COUNT; i++)
+        {
+            if (mediumQualityIndex >= mediumQuality.Count)
+            {
+                mediumQualityIndex = 0;
+                break;
+            }
+
+            targets.Add(mediumQuality[mediumQualityIndex]);
+            mediumQualityIndex++;
+        }
+
+        for (int i = targets.Count; i < MAX_UPDATE_COUNT; i++)
+        {
+            if (highQualityIndex >= highQuality.Count)
+            {
+                highQualityIndex = 0;
+                break;
+            }
+
+            targets.Add(highQuality[highQualityIndex]);
+            highQualityIndex++;
         }
     }
 
-    private void UpdateTargetDepthValues()
+    private void UpdateTargetsDepthValues()
     {
         WaterSimSearchData waterSimSearchData = new();
 
@@ -66,9 +182,14 @@ public class BuoyancyManager : MonoBehaviour
             return;
         }
 
-        for (int i = 0; i < buoyancyPoints.Values.Length; i++)
+        for (int i = 0; i < targets.Count; i++)
         {
-            targetPositionBuffer[i] = targets[currentTarget].transform.position + targets[currentTarget].transform.TransformVector(buoyancyPoints.Values[i]);
+            int offset = buoyancyPoints.Values.Length * i;
+
+            for (int j = 0; j < buoyancyPoints.Values.Length; j++)
+            {
+                targetPositionBuffer[offset + j] = targets[i].transform.position + targets[i].transform.TransformVector(buoyancyPoints.Values[j]);
+            }
         }
 
         WaterSimulationSearchJob waterSimulationSearchJob = new()
@@ -88,28 +209,43 @@ public class BuoyancyManager : MonoBehaviour
             directionBuffer = directionBuffer
         };
 
-        JobHandle jobHandle = waterSimulationSearchJob.Schedule(buoyancyPoints.Values.Length, 1);
+        JobHandle jobHandle = waterSimulationSearchJob.Schedule(buoyancyPoints.Values.Length * MAX_UPDATE_COUNT, 1);
         jobHandle.Complete();
 
-        for (int i = 0; i < buoyancyPoints.Values.Length; i++)
+        for (int i = 0; i < targets.Count; i++)
         {
-            targets[currentTarget].DepthValues[i] = Mathf.Clamp(projectedPositionBuffer[i].y - targetPositionBuffer[i].y, 0, Buoyancy.POINT_SCALE.y) / Buoyancy.POINT_SCALE.y;
+            int offset = buoyancyPoints.Values.Length * i;
+
+            for (int j = 0; j < buoyancyPoints.Values.Length; j++)
+            {
+                targets[i].DepthValues[j] = Mathf.Clamp(projectedPositionBuffer[offset + j].y - targetPositionBuffer[offset + j].y, 0, Buoyancy.POINT_SCALE.y) / Buoyancy.POINT_SCALE.y;
+            }
         }
     }
 
     public void AddTarget(Buoyancy _buoyancy)
     {
-        if (!targets.Contains(_buoyancy))
+        if (!lowQuality.Contains(_buoyancy) && !mediumQuality.Contains(_buoyancy) && !highQuality.Contains(_buoyancy))
         {
-            targets.Add(_buoyancy);
+            lowQuality.Add(_buoyancy);
         }
     }
 
     public void RemoveTarget(Buoyancy _buoyancy)
     {
-        if (targets.Contains(_buoyancy))
+        if (lowQuality.Contains(_buoyancy))
         {
-            targets.Remove(_buoyancy);
+            lowQuality.Remove(_buoyancy);
+        }
+
+        if (mediumQuality.Contains(_buoyancy))
+        {
+            mediumQuality.Remove(_buoyancy);
+        }
+
+        if (highQuality.Contains(_buoyancy))
+        {
+            highQuality.Remove(_buoyancy);
         }
     }
 }
--- Assets/Game/CombatManager/CombatManager.cs
+++ Assets/Game/CombatManager/CombatManager.cs
@@ -15,11 +15,11 @@ public class CombatManager : MonoBehaviour
     private const float SPAWN_SIZE = 1000f;
     private const float SPAWN_BUFFER = 300f;
 
-    private const int MAX_ADMIRAL_COUNT = 1;
+    private const int MAX_ADMIRAL_COUNT = 3;
     private const float SPAWN_COOLDOWN = 30f;
-    private const float SPAWN_PAUSE_DURATION = 0.2f;
-    private const int MIN_FLEET_SIZE = 10;
-    private const int MAX_FLEET_SIZE = 10;
+    private const float SPAWN_PAUSE_DURATION = 0.1f;
+    private const int MIN_FLEET_SIZE = 0;
+    private const int MAX_FLEET_SIZE = 5;
     private CombatManagerSpawnState spawnState = CombatManagerSpawnState.SpawningFirstAdmiral;
 
     private readonly List<EnemyAdmiralController> admirals = new();
