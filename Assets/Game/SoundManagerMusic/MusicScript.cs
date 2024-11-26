e19f4d7f4181b5a3c846d408501714ccba0a0043
Joppeleinen <Joppeleinen@hotmail.se>
2024-11-26 19:07:40 +0100

    added music

---
+++ Assets/Game/SoundManagerMusic/MusicScript.cs
@@ -0,0 +1,63 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicScript : MonoBehaviour
+{
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioClip musicClip;
+    [SerializeField] private float musicVolume;
+    [SerializeField] private float minDelayBetweenTracks;
+    [SerializeField] private float maxDelayBetweenTracks;
+    bool turnOffMusic;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        musicSource.volume = musicVolume;
+        musicSource.clip = musicClip;
+        bool turnOffMusic = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (turnOffMusic == false) 
+        {
+            PlayMusic();
+            if (musicSource.volume != 0.2f) 
+            {
+                musicSource.volume = musicSource.volume + 0.001f;
+            }
+        }
+        if (turnOffMusic == true)
+        {
+            if (musicSource.volume != 0f)
+            {
+                musicSource.volume = musicSource.volume - 0.01f;
+            }
+        }
+    }
+    void PlayMusic() 
+    {
+        if (!musicSource.isPlaying)
+        {
+            musicSource.PlayDelayed(Random.Range(minDelayBetweenTracks, maxDelayBetweenTracks));
+        }
+    }
+    private void Awake()
+    {
+        CombatManager.OnAdmiralInCombatChanged += CombatManager_OnAdmiralInCombatChanged;
+    }
+
+    private void OnDestroy()
+    {
+        CombatManager.OnAdmiralInCombatChanged -= CombatManager_OnAdmiralInCombatChanged;
+    }
+    private void CombatManager_OnAdmiralInCombatChanged(Admiral _admiral)
+    {
+        if (_admiral != null)
+        {
+            turnOffMusic = true;
+        }
+    }
+}
