cd331b206003f2917de137fb490ae248bf45256f
Joppeleinen <Joppeleinen@hotmail.se>
2024-11-28 13:19:15 +0100

    added Audio Pause system

--- Assets/UI/PauseSoundScript.cs
+++ Assets/UI/PauseSoundScript.cs
@@ -4,6 +4,7 @@ public class PauseSoundScript : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip pauseClip;
+    [SerializeField] private AudioSource[] audioSourcesToPause;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
@@ -17,10 +18,23 @@ public class PauseSoundScript : MonoBehaviour
 
     private void UImanager_OnUIStateChange (UIState state) 
     {
-
         if (state == UIState.Pause) 
         {
+            foreach (AudioSource audio in audioSourcesToPause)
+            {
+                audio.Pause();
+            }
             audioSource.PlayOneShot (pauseClip);
+        }
+        if (state == UIState.HUD)
+        {
+            foreach (AudioSource audio in audioSourcesToPause)
+            {
+                if (!audio.isPlaying) 
+                {
+                    audio.Play();
+                }
+            }
         }
     }
 }
