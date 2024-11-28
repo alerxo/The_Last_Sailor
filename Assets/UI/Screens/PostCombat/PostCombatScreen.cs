10708c697348651e204ba2b21a18ae210b28530e
alerxo <alexoorobin@gmail.com>
2024-11-28 20:33:14 +0100

    Update PostCombatScreen.cs

--- Assets/UI/Screens/PostCombat/PostCombatScreen.cs
+++ Assets/UI/Screens/PostCombat/PostCombatScreen.cs
@@ -201,7 +201,7 @@ public class PostCombatScreen : UIScreen
 
             seizeButton.clicked += () => OnSeized(description, _boatController);
             seizeButton.clicked += () => OnBoatSeized?.Invoke();
-            scrapButton.clicked += () => OnScrapped(description, _boatName, _boatController);
+            scrapButton.clicked += () => OnScrapped(description, _boatController);
             scrapButton.clicked += () => OnBoatScrapped?.Invoke();
 
             scrapActions.Add(() => OnScrapped(description, _boatController));
