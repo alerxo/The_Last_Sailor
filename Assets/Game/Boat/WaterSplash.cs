8053887b74386a3c29bfdcba5617e726f6dcb8af
dhamaosta <viktor03.eriksson@gmail.com>
2024-12-01 22:04:18 +0100

    Water splash

---
+++ Assets/Game/Boat/WaterSplash.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+public class WaterSplash : MonoBehaviour
+{
+    [SerializeField] private Transform wave;
+    [SerializeField] private float waveSpeed=3f;
+    [SerializeField] private float waveFallOffSpeed=1f;
+    private WaterDecal[] waterDecals;
+    void Awake()
+    {
+        wave.localScale = new Vector3(0, 1,0);
+        waterDecals = GetComponentsInChildren<WaterDecal>();
+        // Destroy(gameObject,10); // jag kan behöva hjälp med hur man poolar objekt
+    }
+
+
+    void Update()
+    {
+        wave.localScale += new Vector3(waveSpeed*Time.deltaTime, -waveSpeed/10f*Time.deltaTime,waveSpeed*Time.deltaTime);
+        foreach(WaterDecal wd in waterDecals)
+        {
+            float ampl = wd.amplitude;
+            ampl -= waveFallOffSpeed*Time.deltaTime;
+            ampl = Mathf.Clamp(ampl, 0,100);
+            wd.amplitude = ampl;
+        }
+    }
+}
